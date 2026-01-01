7be4b991e3a9a392d8a8250b0408fad2562806fa
HBAhnn <ahn8706@naver.com>
2026-01-02 01:27:14 +0900

    Fix bugs
    # Update handwheel size and pivot
    # Add Door key assets
    # Fix throw item projectile shown other player
    # Update run sound more proper

--- Source/ProjectG/Level/Misc/PGDoor1.cpp
+++ Source/ProjectG/Level/Misc/PGDoor1.cpp
@@ -13,6 +13,8 @@
 #include "Sound/PGSoundManager.h"
 #include "Interface/SoundManagerInterface.h"
 
+#include "GenericTeamAgentInterface.h"
+
 #include "Net/UnrealNetwork.h"
 
 // Sets default values
@@ -98,6 +100,11 @@ void APGDoor1::SetDoorState(bool InbIsOpen, AActor* InteractInvestigator)
 	// Check door open sound twice.
 	// when enemy overlap door, door open called forcely.
 	const bool bOpenTwice = (bIsOpen && InbIsOpen);
+
+	// Player play sound(Door open sound) for both player and enemy. Enemy sound for only player.
+	// Player ID : 0, Enemy ID : 1
+	IGenericTeamAgentInterface* TeamCheckInterface = Cast<IGenericTeamAgentInterface>(InteractInvestigator);
+	bool bIsPlayer = !(TeamCheckInterface->GetGenericTeamId().GetId());
 
 	bIsOpen = InbIsOpen;
 	if (bIsOpen)
